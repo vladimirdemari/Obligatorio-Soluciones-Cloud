84d7bd932a969e9e725cf87ef1765abb038fa653
vladimirdemari <vladimirdemari@gmail.com>
2024-11-27 16:49:27 -0300

    Divido eks.tf en varios componentes y agrego un security group en el cluster para permitir al bastion comunicarse con la API de Kubernetes

---
+++ terraform/modules/eks/cluster_addons.tf
@@ -0,0 +1,21 @@
+# Cluster Add-ons
+resource "aws_eks_addon" "coredns" {
+  cluster_name = aws_eks_cluster.this.name
+  addon_name   = "coredns"
+}
+
+resource "aws_eks_addon" "kube_proxy" {
+  cluster_name = aws_eks_cluster.this.name
+  addon_name   = "kube-proxy"
+}
+
+resource "aws_eks_addon" "vpc_cni" {
+  cluster_name = aws_eks_cluster.this.name
+  addon_name   = "vpc-cni"
+}
+
+resource "aws_eks_addon" "eks_pod_identity_agent" {
+  cluster_name = aws_eks_cluster.this.name
+  addon_name   = "eks-pod-identity-agent"
+}
+
---
+++ terraform/modules/eks/security_groups.tf
@@ -0,0 +1,31 @@
+resource "aws_security_group" "cluster" {
+  name        = "${var.cluster_name}-cluster-sg"
+  description = "Security group for EKS control plane"
+  vpc_id      = var.vpc_id
+
+  ingress {
+    description = "Permitir la comunicacion de los componentes dentro del cluster"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    self        = true
+  }
+
+  ingress {
+    description = "Permitir el acceso a la API de Kubernetes desde el Bastion"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    security_groups = ["sg-03958d92039660269"]
+#    security_groups = [module.bastion.security_group_id]
+  }
+
+  egress {
+    description = "Permitir salida"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
