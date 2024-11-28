2fcf37bb854d95f244f157c9577ccc0fb36d5b67
vladimirdemari <vladimirdemari@gmail.com>
2024-11-28 15:14:30 -0300

    Seguimos intentando slipear el build de docker

--- terraform/modules/docker/docker.tf
+++ terraform/modules/docker/docker.tf
@@ -1,52 +1,52 @@
-# Construir imágenes Docker
-resource "docker_image" "build" {
-  for_each = toset(var.applications)
-
-  # Construye la imagen con el tag `<registry_url>/<app_name>:<commit_short_sha>`
-  name = "${var.registry_url}/${each.key}:${var.commit_short_sha}"
-
-  build {
-    context    = "../boutique-online/src/${each.key}"
-  }
-
-  # Solo se reconstruye si cambia el commit_short_sha
-  triggers = {
-    commit_sha = var.commit_short_sha
-  }
-}
-
-# Crear un tag adicional `latest` para cada imagen
-resource "docker_tag" "latest" {
-  for_each = docker_image.build
-
-  # ID de la imagen construida previamente
-  source_image = each.value.image_id
-
-  # Imagen destino con el tag `latest`
-  target_image = "${var.registry_url}/${each.key}:latest"
-
-  # Ignorar cambios en atributos relacionados con la imagen fuente
-  lifecycle {
-    ignore_changes = [
-      source_image,
-      source_image_id,
-    ]
-  }
-}
-
-# Push del tag `<commit_short_sha>`
-resource "docker_registry_image" "push_sha" {
-
-  for_each = docker_image.build
-
-  name = each.value.name
-}
-
-# Push del tag `latest`
-resource "docker_registry_image" "push_latest" {
-
-  for_each = docker_tag.latest
-
-  name = each.value.target_image
-}
+## Construir imágenes Docker
+#resource "docker_image" "build" {
+#  for_each = toset(var.applications)
+#
+#  # Construye la imagen con el tag `<registry_url>/<app_name>:<commit_short_sha>`
+#  name = "${var.registry_url}/${each.key}:${var.commit_short_sha}"
+#
+#  build {
+#    context    = "../boutique-online/src/${each.key}"
+#  }
+#
+#  # Solo se reconstruye si cambia el commit_short_sha
+#  triggers = {
+#    commit_sha = var.commit_short_sha
+#  }
+#}
+#
+## Crear un tag adicional `latest` para cada imagen
+#resource "docker_tag" "latest" {
+#  for_each = docker_image.build
+#
+#  # ID de la imagen construida previamente
+#  source_image = each.value.image_id
+#
+#  # Imagen destino con el tag `latest`
+#  target_image = "${var.registry_url}/${each.key}:latest"
+#
+#  # Ignorar cambios en atributos relacionados con la imagen fuente
+#  lifecycle {
+#    ignore_changes = [
+#      source_image,
+#      source_image_id,
+#    ]
+#  }
+#}
+#
+## Push del tag `<commit_short_sha>`
+#resource "docker_registry_image" "push_sha" {
+#
+#  for_each = docker_image.build
+#
+#  name = each.value.name
+#}
+#
+## Push del tag `latest`
+#resource "docker_registry_image" "push_latest" {
+#
+#  for_each = docker_tag.latest
+#
+#  name = each.value.target_image
+#}
 
